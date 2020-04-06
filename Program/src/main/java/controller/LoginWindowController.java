1f66035f535b077a036dd383fb409e6c7534b992
Aleksander Sønder <aleksander.soender@gmail.com>
2020-04-06 18:04:58 +0200

    Added logo etc

---
+++ Program/src/main/java/controller/LoginWindowController.java
@@ -0,0 +1,38 @@
+package src.main.java.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Label displayErrorMessage;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void loginButtonClicked() {
+
+    }
+
+    @FXML
+    void passwordAdded() {
+
+    }
+
+    @FXML
+    void usernameAdded() {
+
+    }
+
+
+}
