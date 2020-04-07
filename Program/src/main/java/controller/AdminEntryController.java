05adceb1d83dcec426c90b44e0f1e418b19508da
Aleksander Sønder <aleksander.soender@gmail.com>
2020-04-07 09:19:36 +0200

    EditUser.fxml added + class

--- Program/src/main/java/controller/AdminEntryController.java
+++ Program/src/main/java/controller/AdminEntryController.java
@@ -4,7 +4,7 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class AdminEntryWindowController {
+public class AdminEntryController {
 
 
     @FXML
--- Program/src/main/java/controller/LoginController.java
+++ Program/src/main/java/controller/LoginController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginWindowController {
+public class LoginController {
 
     @FXML
     private TextField usernameTextField;
