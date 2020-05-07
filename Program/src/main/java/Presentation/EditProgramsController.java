c369afb6509b52a7286bc603042f37b67646122e
Simon <sijes16@student.sdu.dk>
2020-05-07 23:19:52 +0200

    Added more functionality

--- Program/src/main/java/Presentation/EditProgramsController.java
+++ Program/src/main/java/Presentation/EditProgramsController.java
@@ -3,8 +3,9 @@ package Presentation;
 
 import Domain.BroadcastType;
 import Domain.Episode;
+import Domain.LiveShow;
+import Domain.Movie;
 import Interfaces.IBroadcast;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,6 +13,7 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -22,8 +24,10 @@ import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class EditProgramsController implements Initializable {
-    Collection<ProgramsData> list;
-    TreeItem<ProgramsData> details;
+    private Collection<ProgramsData> list;
+    private TreeItem<ProgramsData> details;
+    private TreeItem<ProgramsData> root;
+    private int previousIndex;
 
     @FXML
     private TreeTableView<ProgramsData> programTreeTableView;
@@ -116,7 +120,16 @@ public class EditProgramsController implements Initializable {
 
     @FXML
     void deleteButtonOnAction(ActionEvent event) {
+        //See selectProgram method. Some kind of identifier should be saved in ProgramData. This could be broadcastID.
+        //This can be used to find the correct area in the database.
 
+        ProgramsData broadcast = programTreeTableView.getSelectionModel().getSelectedItem().getValue();
+
+        if (broadcast.isMovie() || broadcast.isLiveShow()) {
+            System.out.println(root.getChildren().contains(broadcast));
+            System.out.println("Trying to delete: " + broadcast.getTitle().get());
+            System.out.println(root.getChildren().contains(broadcast));
+        }
     }
 
     @FXML
@@ -126,7 +139,6 @@ public class EditProgramsController implements Initializable {
         if (broadcastTypeComboBox.getSelectionModel().isSelected(1)) {
             App.domain.createMovie(titleTextField.getText(), descriptionTextField.getText(), Year.of(Integer.parseInt(launchDatePicker.getText())));
         }
-
     }
 
     @FXML
@@ -162,6 +174,54 @@ public class EditProgramsController implements Initializable {
     @FXML
     void trailerURLTextFieldOnAction(ActionEvent event) {
 
+    }
+
+
+    @FXML
+    void selectProgram(MouseEvent event) {
+        int currentIndex = programTreeTableView.getSelectionModel().getFocusedIndex();
+
+        //If the same index is selected multiple times it's deselected. Might not work in all situations
+        if (previousIndex == currentIndex) {
+            programTreeTableView.getSelectionModel().clearSelection();
+            previousIndex = -1; //Ensures that you can pick the same index after clearing
+        } else {
+            previousIndex = currentIndex;
+
+            //Check if anything is selected
+            if (programTreeTableView.getSelectionModel().getSelectedItem() != null) {
+
+                //Only check selection if target has no children i.e is not a season or series.
+                System.out.println(programTreeTableView.getSelectionModel().getSelectedItem().getChildren().size());
+                if (programTreeTableView.getSelectionModel().getSelectedItem().getChildren().size() == 0) {
+                    ProgramsData broadcast = programTreeTableView.getSelectionModel().getSelectedItem().getValue();
+
+                    if (broadcast.isEpisode()) {
+                        titleTextField.setText(broadcast.getTitle().get());
+                        broadcastTypeComboBox.getSelectionModel().select(0);
+                        descriptionTextField.setText(broadcast.getBio());
+                        launchDatePicker.setText(broadcast.getYearMade().get());
+
+                    } else if (broadcast.isMovie()) {
+                        titleTextField.setText(broadcast.getTitle().get());
+                        broadcastTypeComboBox.getSelectionModel().select(1);
+                        descriptionTextField.setText(broadcast.getBio());
+                        launchDatePicker.setText(broadcast.getYearMade().get());
+
+                    } else if (broadcast.isLiveShow()) {
+                        titleTextField.setText(broadcast.getTitle().get());
+                        broadcastTypeComboBox.getSelectionModel().select(2);
+                        descriptionTextField.setText(broadcast.getBio());
+                        launchDatePicker.setText(broadcast.getYearMade().get());
+                        locationTextField.setText(broadcast.getLocation());
+
+                    }  else {
+                        System.out.println("Nothing found");
+                        //Do nothing (it's neither, maybe alert user that it's corrupt?)
+                    }
+                }
+            }
+        }
     }
 
     public void getBroadcastData() {
@@ -206,46 +266,49 @@ public class EditProgramsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        TreeItem<ProgramsData> root = new TreeItem<>();
+        //Creates an initial TreeItem which serves as a root. Every new TreeItem should be added to this one.
+        root = new TreeItem<>();
 
+        //An ArrayList containing all broadcasts
         ArrayList<IBroadcast> broadcasts = App.domain.getAllBroadcasts();
 
         for (IBroadcast b : broadcasts) {
-            //Run through the all the series, seasons and episodes. Create any if they haven't already been.
 
+            //Run through the all the series, seasons and episodes. Create any if they haven't already been. Only done for Episodes
             if (b instanceof Episode) {
                 boolean foundSeries = false;
                 int seriesIndex = 0;
 
-                //Run through all series
+                Episode episode = ((Episode) b);
+
+                //Run through all series and check for existing series
                 for (int i = 0; i < root.getChildren().size(); i++) {
-                    System.out.println("Size is: " + root.getChildren().size());
                     //If there is a series with the same name as the showName of an episode check to see if its series exists.
-                    if (root.getChildren().get(i).getValue().getTitle().get().equals(((Episode) b).getShowName())) {
+                    //To convert a SimpleStringProperty to string use .get()
+                    if (root.getChildren().get(i).getValue().getTitle().get().equals(episode.getShowName())) {
                         //If there are no seasons add the one contained in b
                         foundSeries = true;
                         seriesIndex = i;
-                        System.out.println("Found existing series for " + b.getTitle());
+                        System.out.println("Found existing series for " + episode.getTitle());
                     }
                 }
 
+                //If no existing series was found create a new one and find its index.
                 if (!foundSeries) {
-                    root.getChildren().add(new TreeItem<>(new ProgramsData(((Episode) b).getShowName(), "")));
-                    System.out.println(root.getChildren().get(0).getValue().getTitle());
+                    root.getChildren().add(new TreeItem<>(new ProgramsData(episode.getShowName(), "")));
 
                     for (int i = 0; i < root.getChildren().size(); i++) {
-                        //To convert a SimpleStringProperty to string use .get()
-                        if (root.getChildren().get(i).getValue().getTitle().get().equals(((Episode) b).getShowName())) {
+                        if (root.getChildren().get(i).getValue().getTitle().get().equals(episode.getShowName())) {
                             seriesIndex = i;
-                            System.out.println("Creating new series for " + b.getTitle());
+                            System.out.println("Creating new series for " + episode.getTitle());
                         }
                     }
                 }
 
                 //Create a new season if there is none
                 if (root.getChildren().get(seriesIndex).getChildren().isEmpty()) {
-                    root.getChildren().get(seriesIndex).getChildren().add(new TreeItem<>(new ProgramsData(((Episode) b).getSeason(), b.getLaunchYear().toString())));
-                    System.out.println("Series is empty, creating new series for: " + b.getTitle());
+                    root.getChildren().get(seriesIndex).getChildren().add(new TreeItem<>(new ProgramsData(episode.getSeason(), episode.getLaunchYear().toString())));
+                    System.out.println("Series is empty, creating new series for: " + episode.getTitle());
                 }
 
                 //Check if there is an existing season
@@ -253,22 +316,23 @@ public class EditProgramsController implements Initializable {
                     boolean foundSeason = false; //Check if any seasons where found
 
                     for (int j = 0; j < root.getChildren().get(seriesIndex).getChildren().size(); j++) {
-                        if (root.getChildren().get(seriesIndex).getChildren().get(j).getValue().getSeason().equals(Integer.toString(((Episode) b).getSeason()))) {
-                            root.getChildren().get(seriesIndex).getChildren().get(j).getChildren().add(new TreeItem<>(new ProgramsData(b.getTitle(), b.getLaunchYear().toString())));
+                        if (root.getChildren().get(seriesIndex).getChildren().get(j).getValue().getSeason().equals(Integer.toString(episode.getSeason()))) {
+                            root.getChildren().get(seriesIndex).getChildren().get(j).getChildren().add(new TreeItem<>(new ProgramsData(episode.getTitle(), episode.getLaunchYear().toString(),
+                                    Integer.toString(episode.getSeason()), Integer.toString(episode.getEpisodeNum()), episode.getShowName(), episode.getBio())));
                             foundSeason = true;
-                            System.out.println("Found season for: " + b.getTitle());
+                            System.out.println("Found season for: " + episode.getTitle());
                         }
-
                     }
 
                     //If a matching season weren't found create a new one and add the episode
                     if (!foundSeason) {
-                        root.getChildren().get(seriesIndex).getChildren().add(new TreeItem<>(new ProgramsData(((Episode) b).getSeason(), b.getLaunchYear().toString())));
+                        root.getChildren().get(seriesIndex).getChildren().add(new TreeItem<>(new ProgramsData(episode.getSeason(), episode.getLaunchYear().toString())));
 
                         for (int j = 0; j < root.getChildren().get(seriesIndex).getChildren().size(); j++) {
-                            if (root.getChildren().get(seriesIndex).getChildren().get(j).getValue().getSeason().equals(Integer.toString(((Episode) b).getSeason()))) {
-                                root.getChildren().get(seriesIndex).getChildren().get(j).getChildren().add(new TreeItem<>(new ProgramsData(b.getTitle(), b.getLaunchYear().toString())));
-                                System.out.println("Added new season and adding " + b.getTitle());
+                            if (root.getChildren().get(seriesIndex).getChildren().get(j).getValue().getSeason().equals(Integer.toString(episode.getSeason()))) {
+                                root.getChildren().get(seriesIndex).getChildren().get(j).getChildren().add(new TreeItem<>(new ProgramsData(episode.getTitle(), episode.getLaunchYear().toString(),
+                                        Integer.toString(episode.getSeason()), Integer.toString(episode.getEpisodeNum()), episode.getShowName(), episode.getBio())));
+                                System.out.println("Added new season and adding " + episode.getTitle());
 
                             }
                         }
@@ -278,8 +342,12 @@ public class EditProgramsController implements Initializable {
 
                 }
 
-            } else {
-                root.getChildren().add(new TreeItem<>(new ProgramsData(b.getTitle(), b.getLaunchYear().toString())));
+            //If a broadcast is not an Episode it's simply added.
+            } else if (b instanceof Movie){
+                root.getChildren().add(new TreeItem<>(new ProgramsData(b.getTitle(), b.getLaunchYear().toString(), b.getBio())));
+
+            } else if (b instanceof LiveShow) {
+                root.getChildren().add(new TreeItem<>(new ProgramsData(b.getTitle(), b.getLaunchYear().toString(), b.getBio(), ((LiveShow) b).getLocation())));
             }
 
                 /*
@@ -297,18 +365,17 @@ public class EditProgramsController implements Initializable {
                 }
 
                  */
-
-            titleColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<ProgramsData, String> param) -> param.getValue().getValue().getTitle());
-            yearMadeColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<ProgramsData, String> param) -> param.getValue().getValue().getYearMade());
-
-            programTreeTableView.setRoot(root);
-            programTreeTableView.setShowRoot(false);
-            //programTreeTableView.setShowRoot(false);
-
-            //Adding choice options to the broadcast type combobox
-            broadcastTypeComboBox.getItems().setAll(BroadcastType.values());
-
-
         }
+
+        titleColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<ProgramsData, String> param) -> param.getValue().getValue().getTitle());
+        yearMadeColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<ProgramsData, String> param) -> param.getValue().getValue().getYearMade());
+
+        programTreeTableView.setRoot(root);
+        //Root should not be visible
+        programTreeTableView.setShowRoot(false);
+
+        //Adding choice options to the broadcast type combobox
+        broadcastTypeComboBox.getItems().setAll(BroadcastType.values());
+        previousIndex = -1;
     }
 }
--- Program/src/main/java/Presentation/ProgramsData.java
+++ Program/src/main/java/Presentation/ProgramsData.java
@@ -7,18 +7,54 @@ public class ProgramsData {
     SimpleStringProperty title;
     SimpleStringProperty yearMade;
     SimpleStringProperty season;
-    SimpleStringProperty episode;
+    SimpleStringProperty episodeNum;
     SimpleStringProperty showName;
+    String bio;
+    String location;
+    boolean episode = false;
+    boolean liveShow = false;
+    boolean movie = false;
 
+    //Used to create series
     public ProgramsData(String title, String yearMade) {
         this.title = new SimpleStringProperty(title);
         this.yearMade = new SimpleStringProperty(yearMade);
     }
 
+    //Used to create seasons
     public ProgramsData(int season, String yearMade) {
         this.title = new SimpleStringProperty("Sæson " + season);
         this.season = new SimpleStringProperty("" + season);
     }
+
+    //Used to create episodes
+    public ProgramsData(String title, String yearMade, String season, String episodeNum, String showName, String bio) {
+        this.title = new SimpleStringProperty(title);
+        this.yearMade = new SimpleStringProperty(yearMade);
+        this.season = new SimpleStringProperty(season);
+        this.episodeNum = new SimpleStringProperty(episodeNum);
+        this.showName = new SimpleStringProperty(showName);
+        this.bio = bio;
+        this.episode = true;
+    }
+
+    //Used to create liveShows
+    public ProgramsData(String title, String yearMade, String bio, String location) {
+        this.title = new SimpleStringProperty(title);
+        this.yearMade = new SimpleStringProperty(yearMade);
+        this.bio = bio;
+        this.location = location;
+        this.liveShow = true;
+    }
+
+    //Used to create movies
+    public ProgramsData(String title, String yearMade, String bio) {
+        this.title = new SimpleStringProperty(title);
+        this.yearMade = new SimpleStringProperty(yearMade);
+        this.bio = bio;
+        this.movie = true;
+    }
+
 
     public ProgramsData() {}
 
@@ -34,8 +70,8 @@ public class ProgramsData {
         this.season.set(season);
     }
 
-    public void setEpisode(String episode) {
-        this.episode.set(episode);
+    public void setEpisodeNum(String episodeNum) {
+        this.episodeNum.set(episodeNum);
     }
 
     public SimpleStringProperty getTitle() {
@@ -62,16 +98,35 @@ public class ProgramsData {
         return season;
     }
 
-    public String getEpisode() {
-        return episode.get();
+    public String getEpisodeNum() {
+        return episodeNum.get();
     }
 
-    public SimpleStringProperty episodeProperty() {
-        return episode;
+    public SimpleStringProperty episodeNumProperty() {
+        return episodeNum;
     }
 
     public SimpleStringProperty showNameProperty() {return showName;}
 
     public String getShowName() {return showName.get();}
 
+    public boolean isEpisode() {
+        return episode;
+    }
+
+    public boolean isLiveShow() {
+        return liveShow;
+    }
+
+    public boolean isMovie() {
+        return movie;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
